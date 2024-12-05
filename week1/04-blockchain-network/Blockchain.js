8bde5074bc962dab7c9687f748328e12f3ec7309
ddol9 <drddol9@gmail.com>
2024-12-05 19:54:26 +0900

    add 4-blockchain-network

---
+++ week1/04-blockchain-network/Blockchain.js
@@ -0,0 +1,32 @@
+const Block = require("./Block");
+
+class Blockchain {
+    constructor() {
+        const block = new Block();
+        this.chain = [block];
+    }
+
+    addBlock(block) {
+        const previousBlock = this.chain[this.chain.length - 1];
+        block.previousHash = previousBlock.toHash();
+        this.chain.push(block);
+    }
+
+    isValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (
+                currentBlock.previousHash.toString() !==
+                previousBlock.toHash().toString()
+            ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = Blockchain;
